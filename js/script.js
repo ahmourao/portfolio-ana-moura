21d40a8adb21291a6001f936c3fbe761de57c781
Ana Moura <fon2018ana@gmail.com>
2024-06-07 22:06:29 -0300

    Página de Habilidades pronta

--- js/script.js
+++ js/script.js
@@ -41,8 +41,21 @@ document.getElementById('link-projetos-pesquisa-cientifica').addEventListener('c
     document.getElementById('projetos-academicos').classList.add('hidden');
     document.getElementById('projetos-pesquisa-cientifica').classList.remove('hidden');
     document.getElementById('conteudo').classList.add('hidden');
+    document.getElementById('habilidades').classList.add('hidden');
     document.getElementById('projetos-pesquisa-cientifica').scrollIntoView({ behavior: 'smooth' });
 });
+
+// Navega para a seção "Habilidades" e destaca o item de navegação ativo
+document.getElementById('link-habilidades').addEventListener('click', function() {
+    document.querySelector('li.active').classList.remove('active');
+    this.classList.add('active');
+    document.getElementById('sobre-mim').classList.add('hidden');
+    document.getElementById('projetos-academicos').classList.add('hidden');
+    document.getElementById('projetos-pesquisa-cientifica').classList.add('hidden');
+    document.getElementById('habilidades').classList.remove('hidden');
+    document.getElementById('habilidades').scrollIntoView({ behavior: 'smooth' });
+});
+
 
 
 
@@ -50,6 +63,13 @@ document.getElementById('link-projetos-pesquisa-cientifica').addEventListener('c
 document.getElementById('triangle-up').addEventListener('click', function() {
     document.querySelector('header').scrollIntoView({ behavior: 'smooth' });
 });
+
+document.querySelectorAll('.fixed-triangle').forEach(triangle => {
+    triangle.addEventListener('click', function() {
+        document.querySelector('header').scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
 
 
 // Adiciona evento de clique aos botões dos projetos
